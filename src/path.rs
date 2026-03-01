3f04a18f07e5145a4d84255d76e13df33d8c8405
Matej Skalicky <matej.skalickyy@gmail.com>
2026-03-02 00:02:26 +0100

    added path struct

---
+++ src/path.rs
@@ -0,0 +1,32 @@
+use std::{
+    error::Error,
+    path::{Path, PathBuf},
+};
+
+use crate::utils::to_tilde_path;
+
+#[derive(Debug, Ord, Eq, PartialEq, PartialOrd)]
+pub struct ProjectPath {
+    pub tilde: String,
+    pub real: PathBuf,
+    pub sesh_name: String,
+}
+
+impl ProjectPath {
+    pub fn from_path(path: PathBuf) -> Result<Self, Box<dyn Error>> {
+        let tilde = to_tilde_path(&path)?;
+        let sesh_name = last_component(&path)?;
+        Ok(Self {
+            tilde,
+            real: path,
+            sesh_name,
+        })
+    }
+}
+
+fn last_component(path: &Path) -> Result<String, Box<dyn Error>> {
+    path.file_name()
+        .and_then(|n| n.to_str())
+        .map(str::to_owned)
+        .ok_or_else(|| format!("failed to derive name from path: {}", path.display()).into())
+}
